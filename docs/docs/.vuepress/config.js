a3072a71cfaab1e5c9c752b8f3bb611d77bea27f
Mikhail-iontsev <mikhail_iontsev@icloud.com>
2024-10-20 19:28:35 +0400

    test1

--- docs/docs/.vuepress/config.js
+++ docs/docs/.vuepress/config.js
@@ -4,7 +4,7 @@ import { viteBundler } from '@vuepress/bundler-vite'
 
 export default defineUserConfig({
   lang: 'en-US',
-  base: '/<REPO_NAME>/',
+  base: 'ares',
   title: 'Ares',
   description: 'A Research Exploration System',
 
